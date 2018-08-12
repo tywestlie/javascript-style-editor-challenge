8906bb2ef87e84fb69584eda93376851f3364c3d
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-12 17:28:27 -0600

    selectors and css properties working in the browser

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-  // your code goes here.
+$(document).ready( () => {
+    $("input[type=submit]").on( "click", () => {
+      let selector = $(style_editor[name="selector"]).val();
+
+      let property = $(style_editor[name="property"]).val();
+
+      let value = $(style_editor[name="value"]).val();
+
+      $(`${selector}`).css(property, value);
+
+      event.preventDefault();
+    });
 });
